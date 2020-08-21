8cb1d201caf6114c70b3de819b3f22d6aec0d684
gusaiani <gustavo@poe.ma>
2020-08-21 14:19:04 -0300

    Recreate Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
@@ -19,6 +20,41 @@ const GET_PLACES_QUERY = gql`
     }
   }
 `;
+
+const Home = () => (
+  <Query query={GET_PLACES_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <Loading />;
+      if (error) return <Error error={error} />;
+      return <PlaceList places={data.places} />;
+    }}
+  </Query>
+);
+
+export default Home;
+*/
+
+import gql from "graphql-tag";
+import React from 'react';
+import { Query } from "react-apollo";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
+import PlaceList from "../components/PlaceList";
+
+const GET_PLACES_QUERY = gql`
+  query GetPlaces {
+    places {
+      id
+      slug
+      name
+      location
+      description
+      pricePerNight
+      imageThumbnail
+      maxGuests
+    }
+  }
+`
 
 const Home = () => (
   <Query query={GET_PLACES_QUERY}>
